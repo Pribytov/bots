3768424dc908c91cd8fe0fe77c65e51ba89d9b73
Igor Pribytov <igor-pribytov@yandex.ru>
2022-04-20 04:34:55 +0300

    Update yandex_bot.js
    Добавил повторный обход - не находит адреса по ссылке href

--- yandex_bot.js
+++ yandex_bot.js
@@ -4,39 +4,51 @@
 // @version      0.1
 // @description  try to take over the world!
 // @author       Igor Pribytov
-// @match        https://yandex.ru/
+// @match        https://yandex.ru/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
 // ==/UserScript==
 
 
-let keywords = ["купля-продажа авто", "каталог автомобилей", "автомобили купить"];
-
-document.getElementsByName("text")[0].value = "автомобили купить";
-
+//let keywords = ["купля-продажа авто", "каталог автомобилей", "автомобили купить"];
+//let keyword = keywords[getRandom(0, keywords.length)];
 
 document.querySelectorAll(".mini-suggest__button")[0].click();
 
-let timerId = setTimeout(function checkFindButton() {
-
-  if (document.querySelectorAll(".mini-suggest__button")[0].click() == undefined){
-    timerId = setTimeout(checkFindButton, 2000);
-  }
-
-}, 2000);
-
+let btn_find = document.querySelectorAll(".mini-suggest__button")[0];
 
 let links = document.links;
 
-for (let i = 0; i < links.length; i++) {
- if (links[i].href.indexOf("auto.ru") !== -1){
-    let link = links[i];
-    console.log("Нашел строку " + links[i]);
-    //link.removeAttribute("target");
-    //link.click();
-    break;
+if (btn_find != undefined) {
+  //document.getElementsByName("text")[0].value = keyword;
+  document.getElementsByName("text")[0].value = "купля-продажа авто";
+  let timerId = setTimeout(function checkFindButton() {
+
+    if (document.querySelectorAll(".mini-suggest__button")[0].click() == undefined){
+      timerId = setTimeout(checkFindButton, 2000);
+    }
+
+  }, 2000);
+} else {
+  links = document.links;
+  for (let i = 0; i < links.length; i++) {
+  //for (let i = 0; i < 2; i++) {
+   if (links[i].href.indexOf("auto.ru") !== -1){
+   //if (links[i].href.indexOf("https://auto.ru/docs/dkp/") !== -1) {
+      let link = links[i];
+      console.log("Нашел строку " + links[i]);
+      link.removeAttribute("target");
+      link.click();
+      break;
+    }
   }
-}
+ }
+
+//function getRandom(min, max) {
+//  return Math.floor(Math.random() * (max- min) + min);
+//}
+
+
 
 
 
